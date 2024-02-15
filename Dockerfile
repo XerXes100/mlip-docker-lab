fd4997a0225ec5224f62753a6ff461f96419d007
XerXes100 <tirthjain1411@gmail.com>
2024-02-15 12:09:37 -0500

    Completed lab, filled TODOs

--- Dockerfile
+++ Dockerfile
@@ -3,12 +3,17 @@
 FROM python:3.9-slim
 
 # TODO [1]: Set WORKDIR to /app
+WORKDIR /app
 
 # TODO [2.1]: Copy the requirements.txt file to the WORKDIR
+COPY requirements.txt requirements.txt
+COPY iris_model.pkl iris_model.pkl
 
 # TODO [2.2]: Install the requirements
+RUN pip3 install -r requirements.txt
 
 # TODO [3]: Copy the server to the WORKDIR
+COPY server.py server.py
 
 # TODO [4]: Set the command to run the app
-CMD [...]
+CMD ["python3", "server.py"]
--- server.py
+++ server.py
@@ -6,25 +6,28 @@ app = Flask(__name__)
 
 from flask import request
 
-@app.route('/predict', methods=['GET'])
+
+@app.route("/predict", methods=["GET"])
 def predict():
     # Get the input array from the request
     get_json = request.get_json()
-    iris_input = get_json['input']
-    
+    iris_input = get_json["input"]
+
     # TODO: Import trained model
-    model = ...
-    
-    # TODO: Make prediction using the model 
+    model = joblib.load("iris_model.pkl")
+
+    # TODO: Make prediction using the model
     # HINT: use np.array().reshape(1, -1) to convert input to 2D array
-    prediction = ...
-    
+    prediction = model.predict(np.array(iris_input).reshape(1, -1))
+
     # TODO: Return the prediction as a response
-    return ...
+    return {"prediction": prediction[0]}
 
-@app.route('/')
+
+@app.route("/")
 def hello():
-    return 'Welcome to Docker Lab'
+    return "Welcome to Docker Lab"
 
-if __name__ == '__main__':
-    app.run(debug=True, port=8080, host='0.0.0.0')
+
+if __name__ == "__main__":
+    app.run(debug=True, port=8080, host="0.0.0.0")
